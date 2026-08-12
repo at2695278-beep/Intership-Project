30009a4cfc4fda21d0b15239d1eb2828b802607a
Ayush Tiwari <at2695278@gmail.com>
2026-08-12 17:59:25 +0530

    updated

--- Task3/script.js
+++ Task3/script.js
@@ -3,10 +3,10 @@ const buttons = document.querySelectorAll("button");
 for (let i = 0; i < buttons.length; i++) {
      buttons[i].addEventListener("click", function () {
        let value = this.innerText;
-        if (value === "C") {
+        if (value === "AC") {
           display.value = "";
         }
-        else if (value === "⌫") {
+        else if (value === "X") {
           display.value = display.value.slice(0, -1);
         }
         else if (value === "=") {
